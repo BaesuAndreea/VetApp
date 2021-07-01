bd378a340d6c7ef9dfea7f4ecb9225e404181806
AB2010 <baesu.andreea@yahoo.com>
2021-07-01 23:44:07 +0300

    front end pets

---
+++ VetApp/ClientApp/src/app/pet/models/pet.ts
@@ -0,0 +1,17 @@
+export class Pet {
+  id: number;
+  name: string;
+  species: string;
+  breed: string;
+  age: number;
+  owner: string;   
+}
+
+export const SPECIES = [
+  'cat',
+  'dog',
+  'rodent',
+  'bird',
+  'other'
+
+];
---
+++ VetApp/ClientApp/src/app/pet/pet-list/pet-list.component.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Pet, SPECIES } from '../models/pet';
+
+@Component({
+  selector: 'app-pet-list',
+  templateUrl: './pet-list.component.html',
+  styleUrls: ['./pet-list.component.css']
+})
+export class PetListComponent implements OnInit {
+
+  public pets: Pet[];
+  public pet: Pet = new Pet();
+  errors;
+  public url: string;
+  SPECIES = SPECIES;
+
+  constructor(private http: HttpClient, @Inject('API_URL') private apiUrl: string) {
+    this.url = `${this.apiUrl}pets`;
+    console.log(this.url);
+    this.loadPets();
+  }
+
+  loadPets() {
+    this.http.get<Pet[]>(this.url).subscribe(result => {
+      console.log(result);
+      this.pets = result;
+      this.errors = "";
+    }, (err) => {
+      this.errors = JSON.stringify(err);
+    })
+  }
+
+  addPet() {
+    this.http.post(this.url, this.pet).subscribe(
+      () => { this.loadPets(); this.errors = ""; },
+      (err) => {
+        this.errors = JSON.stringify(err);
+      })
+  }
+
+  updatePet(pet: Pet) {
+    this.http.put(`${this.url}/${pet.id}`, pet).subscribe(result => {
+      this.errors = "";
+    }, (err) => {
+      this.errors = JSON.stringify(err);
+    })
+  }
+
+  deletePet(id: number) {
+    this.http.delete(`${this.url}/${id}`).subscribe(
+      () => { this.loadPets(); this.errors = ""; },
+      (err) => {
+        this.errors = JSON.stringify(err);
+      })
+  }
+
+  ngOnInit() {
+  }
+
+}
