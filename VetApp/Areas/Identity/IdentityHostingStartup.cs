5132735a1a5a9886fd623fcb61d44d15af463a14
AB2010 <baesu.andreea@yahoo.com>
2021-07-02 13:11:52 +0300

    Add Scaffolding

---
+++ VetApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VetApp.Data;
+using VetApp.Models;
+
+[assembly: HostingStartup(typeof(VetApp.Areas.Identity.IdentityHostingStartup))]
+namespace VetApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
